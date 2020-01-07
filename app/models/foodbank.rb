b9984e5b2d882db474c4ea17fa6b7eaff9dfbb4d
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-01-07 09:36:13 +0000

    add geocode filter to foodbank

--- app/models/foodbank.rb
+++ app/models/foodbank.rb
@@ -1,6 +1,12 @@
 class Foodbank < ApplicationRecord
+  before_save :geocode
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def geocode
+    # get lat and lng columns from address here
+  end
+
 end
