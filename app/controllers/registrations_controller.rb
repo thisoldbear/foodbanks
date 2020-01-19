a67a2b5bc2641b47e04fd3a9cc58d189c7fab25d
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-01-19 23:29:42 +0000

    password behaviour is correct

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,7 +3,7 @@ class RegistrationsController < Devise::RegistrationsController
     protected
 
     def update_resource(resource, params)
-        if params[:password].blank? && params[:password_confirmation].blank?
+        if params[:password].blank? && params[:password_confirmation].blank? && params[:email] === current_user.email
             resource.update_without_password(params)
         else
             super
